2c447d4e664a16ac2df7e85cfe725012d94f3093
Alyssa <78221325+lawwalyy@users.noreply.github.com>
2021-03-14 16:45:02 +0800

    Add files via upload

---
+++ alyssa.js
@@ -0,0 +1,9 @@
+<html>
+   <body>   
+      <script language = "javascript" type = "text/javascript">
+         <!--
+            document.write("Hello! This is Alyssa Laurence")
+         //-->
+      </script>      
+   </body>
+</html>
