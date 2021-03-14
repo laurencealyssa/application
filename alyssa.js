bcf6de361b9fd18fc7235ca32438ebbee2d8b159
Alyssa <78221325+lawwalyy@users.noreply.github.com>
2021-03-14 16:50:46 +0800

    Update alyssa.js

--- alyssa.js
+++ alyssa.js
@@ -1,9 +1,19 @@
-<html>
-   <body>   
-      <script language = "javascript" type = "text/javascript">
-         <!--
-            document.write("Hello! This is Alyssa Laurence")
-         //-->
-      </script>      
-   </body>
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF+8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Today's Date</title>
+</head>
+
+<body>
+
+  <script>
+      let d = new Date();
+      document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+  </script>
+
+</body>
+
 </html>
