f7436d6a19a60d4b8e26e6820751ce3030ae3360
Alyssa <78221325+lawwalyy@users.noreply.github.com>
2021-03-14 21:13:15 +0800

    Update web.py

--- web.py
+++ web.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return 'Hello, Welcome to the sample deployment of Alyssa Laurence from Heroku'
+  return 'Hello, Welcome to the sample deployment of Alyssa Laurence of BSIT-2 from Heroku Application.'
